a970d2ca6e07bc9cd74394d8c5141706b35a53d8
NikhilPrasad0497 <nikhilpd791@gmail.com>
2021-03-22 01:03:40 -0700

    Feat: USEReducer Practice

---
+++ usereducer/src/App.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line
+// import logo from './logo.svg';
+import { useReducer} from 'react'
+import './App.css';
+
+function App() {
+  // const recuderfunc = (state, value)=>{
+  //   // console.log(`ha ha dikh rha hain, antenna pakde rahiye`, value, state.initialNum)
+  //   return {...state, count: state.counter+1}
+  // }
+  // // const [ counter, setcounter] = useState(0)
+  // // const counter = 0;
+  const counterReducer = (acc, dispatchValue)=> {
+    // console.log(acc, dispatchValue)
+    switch(dispatchValue.type){
+      case "INCREMENT":
+        return {...acc, counter:acc.counter+1}
+      case "DECREMENT":
+        return {...acc, counter:acc.counter+1}
+      default:
+        console.log("code Broke")
+        break
+    }
+    return state;
+  }
+  const [ state, dispatch] = useReducer(()=> counterReducer,{ counter: 0})
+  return (
+    <div className="App">
+      <h1>Counter</h1>
+      <div>Count: {state.counter}</div>
+      <button onClick={()=> dispatch({type: "INCREMENT"})}> + </button>
+      <button onClick={()=> dispatch({type: "DECREMENT"})}> - </button>
+    </div>
+  );
+}
+
+export default App;
